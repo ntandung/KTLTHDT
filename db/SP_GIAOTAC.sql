55230270eae03ce5bb4d771943ea326929ca1f0e
Dung Nguyen <tandung847@gmail.com>
2019-10-09 21:35:01 +0700

    QLDSV: SP_GIAOTAC

--- db/SP_GIAOTAC.sql
+++ db/SP_GIAOTAC.sql
@@ -1,33 +1,28 @@
-CREATE PROC [SP_TIMSUPPORT]
+CREATE PROC [SP_GIAOTAC]
 @minsup float
 AS
 BEGIN
 	
 	select MAMH,COUNT(DISTINCT(MASV))AS sup INTO #DSMAMH_Sup from (select DISTINCT(MASV), MAMH from DIEM group by MAMH, MASV HAVING MAX(DIEM)<5) AS tmp group by MAMH
-	-- select MAMH,COUNT(DISTINCT(MASV))AS sup INTO #DSMAMH_Sup from DIEM group by MAMH, MASV HAVING MAX(DIEM)<5
 	DECLARE @SUP float
-	DECLARE @minsup float
-	SET @minsup = 50
 	SET @SUP  =(@minsup * (SELECT COUNT(*) FROM (SELECT DISTINCT(MASV) FROM DIEM group by MAMH, MASV HAVING MAX(DIEM)<5) AS tmp))/100.0
-	PRINT @SUP
 	
 	SELECT DISTINCT(MAMH) INTO #DSMAMH FROM #DSMAMH_Sup 
-	WHERE sup >= @SUP ORDER BY (MAMH)
+	WHERE SUP >= @SUP ORDER BY (MAMH)
 	
 	DECLARE @PivotColumnHeader varchar(max)
 	SET @PivotColumnHeader = ''
 	SELECT @PivotColumnHeader += RTRIM(MAMH) + ',' FROM #DSMAMH
 	SET @PivotColumnHeader = SUBSTRING(@PivotColumnHeader,1,LEN(@PivotColumnHeader)-1)
 	 
-	SELECT MASV,MAMH into #DIEM from DIEM 
+	SELECT MASV,MAMH into #DIEM from DIEM group by MAMH, MASV HAVING MAX(DIEM)<5
 	DECLARE @PivotTable varchar(MAX)
 	SET @PivotTable = 'SELECT * FROM #DIEM Pivot( COUNT(MAMH) for MAMH in ('+ @PivotColumnHeader +')) as PivotTable'
 	PRINT (@PivotTable)
 	execute(@PivotTable)
 
-
 	DROP TABLE #DSMAMH_Sup
 	DROP TABLE #DSMAMH
-	DROP TABLE #Diem
+	DROP TABLE #DIEM
 	
 END
