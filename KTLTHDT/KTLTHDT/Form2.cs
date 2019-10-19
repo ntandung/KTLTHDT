36ce6450ef664c2b513b30e458ae24b412ef49fc
Dung Nguyen <tandung847@gmail.com>
2019-10-19 17:39:58 +0700

    WIP: Form2 handle btnBack

--- KTLTHDT/KTLTHDT/Form2.cs
+++ KTLTHDT/KTLTHDT/Form2.cs
@@ -22,6 +22,7 @@ namespace KTLTHDT
             Program.tapNMuc = 0;
             Program.tapF.RemoveRange(1, Program.tapF.Count - 1);
             Program.tapL.Clear();
+            btnBack.Enabled = false;
 
             DataTable dt = new DataTable();
             dt.Columns.Add("TID");
@@ -53,7 +54,8 @@ namespace KTLTHDT
 
         private void btnNext_Click(object sender, EventArgs e)
         {
-            if (btnNext.Text.Equals("Finish"))
+            btnBack.Enabled = true;
+            if (btnNext.Text.Equals("FINISH"))
             {
                 Form3 form3 = new Form3();
                 form3.ShowDialog();
@@ -86,10 +88,52 @@ namespace KTLTHDT
 
                 if (Program.tinhC(Program.tapF[Program.tapNMuc]).Count == 0)
                 {
-                    btnNext.Text = "Finish";
+                    btnNext.Text = "FINISH";
                 }
             }
             
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+
+            Program.tapNMuc -= 1;
+            DataTable dt = new DataTable();
+            dt.Columns.Add("TID");
+            dt.Columns.Add("Tập các mục");
+            foreach (var oItem in Program.tapF[Program.tapNMuc])
+            {
+                dt.Rows.Add(new object[] { oItem.Key, Program.getTapMuc(oItem.Value) });
+            }
+
+            dgvF.DataSource = dt;
+
+
+            DataTable dtc = new DataTable();
+            dtc.Columns.Add("Tập " + (Program.tapNMuc + 1) + " mục");
+            dtc.Columns.Add("Support");
+            var tapC = Program.tinhC(Program.tapF[Program.tapNMuc]);
+            foreach (var oItem in tapC)
+            {
+
+                dtc.Rows.Add(new object[] { Program.getChiMuc(Program.strToInt(oItem.Key)), oItem.Value });
+                Program.tapL[oItem.Key] = oItem.Value;
+            }
+
+            dgvC.DataSource = dtc;
+
+            if (tapC.Count == 0)
+            {
+                btnNext.Text = "FINISH";
+            }
+            else
+            {
+                btnNext.Text = "NEXT";
+            }
+            if (Program.tapNMuc == 0)
+            {
+                btnBack.Enabled = false;
+            }
         }
     }
 }
